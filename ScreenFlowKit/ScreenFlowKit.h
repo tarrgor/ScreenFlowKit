70c53503367ec8c97ed8b52e6a4728b679e9c8c6
Dr. Swifty <drswifty@outlook.de>
2016-12-04 10:54:34 +0100

    Initial commit

---
+++ ScreenFlowKit/ScreenFlowKit.h
@@ -0,0 +1,19 @@
+//
+//  ScreenFlowKit.h
+//  ScreenFlowKit
+//
+//  Created by Thorsten Klusemann on 04.12.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScreenFlowKit.
+FOUNDATION_EXPORT double ScreenFlowKitVersionNumber;
+
+//! Project version string for ScreenFlowKit.
+FOUNDATION_EXPORT const unsigned char ScreenFlowKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenFlowKit/PublicHeader.h>
+
+
